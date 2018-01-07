e31ebc99796bd6cb2f9ddbbbbfe773a165d2cce0
Łukasz Gandecki <lgandecki@css.edu>
2018-01-07 22:52:03 +0400

    simulate mongodb native driver insert

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ module.exports = function (collectionName = 'collectionName') {
   const collection = db.collection(collectionName)
 
   const newCollection = {}
-  const methods = ['insert', 'findOne', 'count', 'update', 'remove']
+  const methods = ['findOne', 'count', 'update', 'remove']
 
   methods.forEach(method => {
     newCollection[method] = thenify(collection[method].bind(collection))
   })
 
+  newCollection.insert = (...args) => {
+    const originalInsert = thenify(collection.insert.bind(collection))
+    return originalInsert(...args).then((res) => {
+      const ids = res.map(doc => doc._id)
+      //TODO the n might be wrong, also, how does this work with upsert?
+      return { result: { ok: res.length, n: res.length }, ops: res, insertedCount: res.length, insertedIds: ids }
+    })
+  }
   newCollection.find = (...args) => {
     const cursor = collection.find(...args)
     let methods = ['toArray', 'skip']
