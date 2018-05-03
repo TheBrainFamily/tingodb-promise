70ed47d6881654cf600fb90e3660a41dc4ae458e
Łukasz Gandecki <lgandecki@css.edu>
2018-05-03 19:03:49 +0400

    limit functionality

--- tests/limit.spec.js
+++ tests/limit.spec.js
@@ -4,26 +4,31 @@ const testCollection = collection('testCollection')
 const testData = [
   {
     _id: 'testData1',
-    test: 'test1'
+    score: 2
   },
   {
     _id: 'testData2',
-    test: 'test2'
+    score: 1
   },
   {
     _id: 'testData3',
-    test: 'test3'
+    score: 3
   },
 ]
+beforeAll(async() => {
+  await Promise.all(
+    testData.map(async data => {
+      testCollection.insert(data)
+    })
+  )
+})
 
-describe('limit', () => {
-  it('limits documents received in find', async () => {
-    await Promise.all(
-      testData.map(async data => {
-        testCollection.insert(data)
-      })
-    )
-    const result = await testCollection.find({}).limit(2).toArray()
-    expect(result.length).toEqual(2)
-  })
+test('find -> limit -> toArray', async () => {
+  const result = await testCollection.find({}).limit(2).toArray()
+  expect(result.length).toEqual(2)
+})
+
+test('find -> sort -> limit -> toArray', async() => {
+  const result = await testCollection.find({}).sort({'score': -1}).limit(1).toArray()
+  expect(result[0].score).toEqual(3)
 })
